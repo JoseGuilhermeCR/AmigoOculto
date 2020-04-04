2cb1e47387eb7db96f9b62f898d6473311f397e1
Sandor Scog <sandorscog@gmail.com>
2020-04-04 18:28:34 -0300

    menu organizing

--- Sandor/Menus.java
+++ Sandor/Menus.java
@@ -3,14 +3,44 @@ import java.util.Scanner;
 
 class Menus {
 
-  CRUD<Usuario> crudUser; 
+  CRUD<Usuario> crudUser;
 
-  public static void telaMenu(){
-    System.out.print("AMIGO OCULTO\n================\n\n");
-    System.out.print("ACESSO\n\n1 - acesso ao sistema\n2 - Novo usuario(primeiro acesso)\n0 - Sair\n\nopcao: ");
+  public void telaMenu(Scanner in){
+
+    inicio(false);
+    int acesso = in.nextInt();
+    while(acesso != 0){
+      Usuario user;
+      //CRUD<Usuario> crudUser;
+      try{
+        crudUser = new CRUD<>("data", Usuario.class.getConstructor());
+
+        while(acesso < 0 || acesso > 2){
+          inicio(true);
+          acesso = in.nextInt();
+        }
+        in.nextLine(); //"fflush()"
+
+        if(acesso == 1){
+          login(in);
+        } else if(acesso == 2){
+          cadastro(in);
+        }
+
+      }catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      if(acesso != 0){
+        inicio(false);
+        acesso = in.nextInt();
+      }
+
+    }
+
   }
 
-  public static void cadastro(Scanner in, CRUD<Usuario> crudUser){
+  public void cadastro(Scanner in){
     try{
       Usuario user = new Usuario();
       boolean flag = false;
@@ -42,14 +72,14 @@ class Menus {
         }//else if
 
       }//while
-          //System.out.println(user.getEmail());
+      //System.out.println(user.getEmail());
     }catch (Exception e){
       e.printStackTrace();
     }
 
   }
 
-  public static void login(Scanner in, CRUD<Usuario> crudUser){
+  public void login(Scanner in){
     try{
       System.out.println("\n\nACESSO AO SISTEMA\n");
       System.out.print("Email: ");
@@ -67,7 +97,7 @@ class Menus {
           System.out.print("\nSenha: ");
           String senha = in.nextLine();
           if(senha.equals(user.getSenha())){
-            telaPrincipal();
+            telaPrincipal(in);
             flag = true;
           } else {
             System.out.println("\nSenha incorreta!!!");
@@ -84,9 +114,82 @@ class Menus {
 
   }
 
-  public static void telaPrincipal(){
-    System.out.println("entrou");
+  public void telaPrincipal(Scanner in){
+
+    menuUsuario(false);
+    int acesso = in.nextInt();
+    while(acesso != 0){
+      Usuario user;
+      //CRUD<Usuario> crudUser;
+      try{
+        crudUser = new CRUD<>("data", Usuario.class.getConstructor());
+
+        while(acesso < 0 || acesso > 3){
+          menuUsuario(true);
+          acesso = in.nextInt();
+        }
+        in.nextLine(); //"fflush()"
+
+        if(acesso == 1){
+          sugestao(in);
+        } else if(acesso == 2){
+          grupos(in);
+        } else if(acesso == 3){
+          convites(in);
+        }
+
+      }catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      if(acesso != 0){
+        menuUsuario(false);
+        acesso = in.nextInt();
+      }
+
+    }
   }
 
+  public void sugestao(Scanner in){
 
+  }
+
+  public void grupos(Scanner in){
+
+  }
+
+  public void convites(Scanner in){
+
+  }
+
+  //----------------------------------------------------------------------------
+  //Static methods that print the menus
+  public static void inicio(boolean invalida){
+
+    if(!invalida)
+      System.out.print("AMIGO OCULTO\n================\n\n" + "ACESSO\n\n" +
+                       "1 - acesso ao sistema\n2 - Novo usuario(primeiro acesso)\n" +
+                       "0 - Sair\n\nopcao: ");
+    else
+      System.out.print("\n------------------------------------------------\n" +
+                       "\n!!!opcao invalida!!!\n\n1 - acesso ao sistema\n" +
+                       "2 - Novo usuario(primeiro acesso)\n0 - Sair\n\nopcao: ");
+  }
+
+  public static void menuUsuario(boolean invalida){
+
+    if(!invalida)
+      System.out.print("AMIGO OCULTO 1.0\n" + "============================\n\n" +
+                      "INICIO:\n\n" + "1) Sugestao de presentes\n" +
+                      "2) Grupos\n" + "3) Novos Convites\n\n" +
+                      "0) Sair\n\n" + "Opcao: ");
+    else
+      System.out.print("Opcao Invalida!!!\n\n" + "1) Sugestao de presentes\n" +
+                      "2) Grupos\n" + "3) Novos Convites\n\n" +
+                      "0) Sair\n\n" + "Opcao: ");
+  }
+
+  public static void menuSugestao(){
+
+  }
 }
--- Sandor/main.java
+++ Sandor/main.java
@@ -6,38 +6,9 @@ import java.util.Scanner;
 
     Scanner in = new Scanner(System.in);
     Menus menus = new Menus();
-    Menus.telaMenu();
+    menus.telaMenu(in);
     Sugestao i = new Sugestao();
-    int acesso = in.nextInt();
-    while(acesso != 0){
-      Usuario user;
-      //CRUD<Usuario> crudUser;
-      try{
-        menus.crudUser = new CRUD<>("data", Usuario.class.getConstructor());
 
-        while(acesso < 0 || acesso > 2){
-          System.out.println("\n------------------------------------------------");
-          System.out.print("\n!!!opcao invalida!!!\n\n1 - acesso ao sistema\n2 - Novo usuario(primeiro acesso)\n0 - Sair\n\nopcao: ");
-          acesso = in.nextInt();
-        }
-        in.nextLine(); //"fflush()"
-
-        if(acesso == 1){
-          Menus.login(in, menus.crudUser);
-        } else if(acesso == 2){
-          Menus.cadastro(in, menus.crudUser);
-        }
-
-      }catch (Exception e) {
-        e.printStackTrace();
-      }
-
-      if(acesso != 0){
-        Menus.telaMenu();
-        acesso = in.nextInt();
-      }
-
-    }
     //System.out.println(user.getNome());
   }
 

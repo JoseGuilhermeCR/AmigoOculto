117e3586e9113dc5eba040deb13038ac1af73bb5
JoseGuilhermeCR <joseguilhermebh@hotmail.com>
2020-05-09 20:12:57 -0300

    Possível visualizar outros participantes de um grupo do qual você participa e de visualizar seu amigo sorteado (não é possível testar ainda.)

--- Jose/java/ui/ParticipacaoUI.java
+++ Jose/java/ui/ParticipacaoUI.java
@@ -1,11 +1,11 @@
 package ui;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 import entidades.Grupo;
 import entidades.Participacao;
 import entidades.Usuario;
+import entidades.Sugestao;
 import infraestrutura.ArvoreBMais_Int_Int;
 import infraestrutura.CRUD;
 import infraestrutura.Infraestrutura;
@@ -13,16 +13,26 @@ import utils.Utils;
 
 public class ParticipacaoUI extends BaseUI {
 
+	private CRUD<Usuario> crudUsuario;
 	private CRUD<Grupo> crudGrupo;
 	private CRUD<Participacao> crudParticipacoes;
+	private CRUD<Sugestao> crudSugestao;
+
 	private ArvoreBMais_Int_Int arvoreUsuarioParticipacao;
+	private ArvoreBMais_Int_Int arvoreGrupoParticipacao;
+	private ArvoreBMais_Int_Int arvoreUsuarioSugestao;
 
 	public ParticipacaoUI(Infraestrutura infraestrutura) {
 		super(infraestrutura);
 
+		crudUsuario = infraestrutura.getCrudUsuario();
 		crudGrupo = infraestrutura.getCrudGrupo();
 		crudParticipacoes = infraestrutura.getCrudParticipacoes();
+		crudSugestao = infraestrutura.getCrudSugestao();
+
 		arvoreUsuarioParticipacao = infraestrutura.getArvoreUsuarioParticipacao();
+		arvoreGrupoParticipacao = infraestrutura.getArvoreGrupoParticipacao();
+		arvoreUsuarioSugestao = infraestrutura.getArvoreUsuarioSugestao();
 	}
 
 	public Resultado telaPrincipalParticipacao(Usuario usuario) {
@@ -58,6 +68,15 @@ public class ParticipacaoUI extends BaseUI {
 				switch (opcao) {
 					case 0:
 						resultado.setSucesso("PARTICIPAÇÃO EM GRUPO > GRUPOS");
+						break;
+					case 1:
+						resultado = telaVisualizarParticipantes(usuario, grupoEscolhido);
+						break;
+					case 2:
+						resultado = telaVisualizarAmigoSorteado(usuario, grupoEscolhido);
+						break;
+					case 3:
+						//resultado = telaLerEnviarMensagens(usuario, grupoEscolhido);
 					default:
 						resultado.setErro("Opção (" + opcao + ") inválida.");
 				}
@@ -108,6 +127,68 @@ public class ParticipacaoUI extends BaseUI {
 			}
 		} else {
 			resultado.setErro("Você não está nenhum grupo.");
+		}
+
+		return resultado;
+	}
+
+	private Resultado telaVisualizarParticipantes(Usuario usuario, Grupo grupo) {
+		Resultado resultado = new Resultado();
+
+		resultado = infraestrutura.listarRelacao1N(grupo, crudParticipacoes, arvoreGrupoParticipacao);
+		ArrayList<Participacao> participacoes = (ArrayList<Participacao>) resultado.getObjeto();
+
+		if (resultado.valido() && participacoes != null && participacoes.size() != 0) {
+			Utils.limpaTela();
+			System.out.println("PARTICIPANTES DE " + grupo.getNome() + "\n");
+
+			for (Participacao participacao : participacoes) {
+				Usuario u = crudUsuario.read(participacao.getIDUsuario());
+				if (u != null) {
+					System.out.println(u.getNome() + " - " + u.getEmail() + ((usuario.getID() == u.getID()) ? " (Você)" : ""));
+				}
+			}
+
+			System.out.print("Pressione enter para continuar...");
+			Utils.scanner.nextLine();
+		}
+
+		return resultado;
+	}
+
+	private Resultado telaVisualizarAmigoSorteado(Usuario usuario, Grupo grupo) {
+		Resultado resultado = new Resultado();
+
+		if (grupo.isSorteado()) {
+			Participacao participacao = crudParticipacoes.read(usuario.getID() + "|" + grupo.getID());
+
+			Usuario amigo = crudUsuario.read(participacao.getIDAmigo());
+
+			Utils.limpaTela();
+			System.out.println("AMIGO SORTEADO:\n\n");
+			System.out.println(amigo.getNome());
+			System.out.println("\nSugestões de presente do amigo:");
+
+			resultado = infraestrutura.listarRelacao1N(amigo, crudSugestao, arvoreUsuarioSugestao);
+			if (resultado.valido()) {
+				ArrayList<Sugestao> sugestoes = (ArrayList<Sugestao>) resultado.getObjeto();
+
+				if (sugestoes.size() != 0) {
+					for (Sugestao sugestao : sugestoes) {
+						sugestao.prettyPrint();
+						System.out.println();
+					}
+				} else {
+					System.out.println(amigo.getNome() + " não cadastrou nenhuma sugestão de presente.\n");
+				}
+			} else {
+				System.out.println("Ocorreu um erro ao buscar as sugestões de " + amigo.getNome());
+			}
+
+			System.out.print("Pressione enter para continuar...");
+			Utils.scanner.nextLine();
+		} else {
+			resultado.setErro("O sorteio deste grupo ainda não aconteceu.");
 		}
 
 		return resultado;
